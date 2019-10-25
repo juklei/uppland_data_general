942170a2b36e85be355714bbe98a490aaec9cc5e
Julian Klein <jg.klein@protonmail.com>
2019-10-25 14:12:29 +0200

    post-experimental data implemented

--- scripts/uppland_forest.R
+++ scripts/uppland_forest.R
@@ -31,10 +31,10 @@ library(rgeos)
 dir()
 forest <- read.csv("data/uppland_forest.csv")
 laser <- read.csv("data/uppland_forest_laser.csv")
-coord <- read.csv("data/coordinates_from_master_file.csv")
+ffmf <- read.csv("data/forgotten_forest_master_file.csv")
 
 head(forest)
-head(coord)
+head(ffmf)
 head(laser)
 
 ## 3. Process forest data ------------------------------------------------------
@@ -107,79 +107,103 @@ laser <- unique(laser[, c("plot", "laser_mean", "experiment")])
 
 ## 5. Create remote sensing data -----------------------------------------------
 
-## Specify directory in which the .asc/.tif files are found and to be stored:
-dir_rs <- "N:/Uppland project/Remote sensing data/"
-dir(dir_rs)
-
-## Lidar File names
-n <- c("ElevP95", "PercentAbove0.5m", "PercentAbove2m", "PercentAbove3m", 
-       "PercentAbove5m", "PercentAbove7m")
-
-## Load Lidar base files into a raster stack:
-base <- stack(paste0(dir_rs, "all_plots_", n, ".asc"))
-names(base) <- n
-
-## Add undergrowth density:
-base$PercentPntrt2m <- 100 - base[["PercentAbove2m"]]
-base$PercentBelow3m <- base[["PercentAbove0.5m"]] -  base[["PercentAbove3m"]]
-base$PercentBelow5m <- base[["PercentAbove0.5m"]] -  base[["PercentAbove5m"]]
-base$PercentBelow7m <- base[["PercentAbove0.5m"]] -  base[["PercentAbove7m"]]
-
-## Store length of red for later use
-lr <- length(base[1])
-
-## Extract data:
-
-buffer <- c(10, 50) ## Chose extraction buffers here
-
-## Create data set which will be filled by the loop:
-
-extracted <- cbind(coord,
-                   matrix(NA, ncol = lr),
-                   sort(rep(buffer, nrow(coord))))
-colnames(extracted)[6:length(extracted)] <- c(names(base), "buffer")
-
-## Run the loop across all buffers (time consuming):
-
-for (i in buffer) {
-
-  bol1 <- extracted$buffer == i
-  extracted[bol1, 6:(5+lr)] <- extract(base,
-                                       extracted[bol1, c("east", "north")],
-                                       fun = mean,
-                                       na.rm = TRUE,
-                                       buffer = i)
-
-}
-
-## Import the stem diameter data:
-sdbh_rs <- raster(paste0(dir_rs, "sksSkogligaGrunddataDgv03.tif"))
-
-## Add the stem diameter to the extracted file:
-for (i in buffer) {
-  
-  bol1 <- extracted$buffer == i
-  extracted$sdbh_rs[bol1] <- extract(sdbh_rs,
-                                     extracted[bol1, c("east", "north")],
-                                     fun = mean,
-                                     na.rm = TRUE,
-                                     buffer = i)
-  
-}
-
-dir.create("temp")
-write.csv(extracted, "temp/forest_RS_uppland.csv", row.names = FALSE)
+# ## Specify directory in which the .asc/.tif files are found and to be stored:
+# dir_rs <- "N:/Uppland project/Remote sensing data/"
+# dir(dir_rs)
+# 
+# ## Lidar File names
+# n <- c("ElevP95", "PercentAbove0.5m", "PercentAbove2m", "PercentAbove3m", 
+#        "PercentAbove5m", "PercentAbove7m")
+# 
+# ## Load Lidar base files into a raster stack:
+# base <- stack(paste0(dir_rs, "all_plots_", n, ".asc"))
+# names(base) <- n
+# 
+# ## Add undergrowth density:
+# base$PercentPntrt2m <- 100 - base[["PercentAbove2m"]]
+# base$PercentBelow3m <- base[["PercentAbove0.5m"]] -  base[["PercentAbove3m"]]
+# base$PercentBelow5m <- base[["PercentAbove0.5m"]] -  base[["PercentAbove5m"]]
+# base$PercentBelow7m <- base[["PercentAbove0.5m"]] -  base[["PercentAbove7m"]]
+# 
+# ## Store length of red for later use
+# lr <- length(base[1])
+# 
+# ## Extract data:
+# 
+# buffer <- c(10, 50) ## Chose extraction buffers here
+# 
+# ## Create data set which will be filled by the loop:
+# 
+# ## Make coordinates file from ffmf:
+# coord <- ffmf[is.na(ffmf$insect_trap), c(1:2,7,12:13)]
+# colnames(coord)[3] <- "circle_10m"
+# coord$circle_10m <- as.character(coord$circle_10m)
+# coord$circle_10m[is.na(coord$circle_10m)] <- "middle"
+# 
+# extracted <- cbind(coord,
+#                    matrix(NA, ncol = lr),
+#                    sort(rep(buffer, nrow(coord))))
+# colnames(extracted)[6:length(extracted)] <- c(names(base), "buffer")
+# 
+# ## Run the loop across all buffers (time consuming):
+# 
+# for (i in buffer) {
+# 
+#   bol1 <- extracted$buffer == i
+#   extracted[bol1, 6:(5+lr)] <- extract(base,
+#                                        extracted[bol1, c("east", "north")],
+#                                        fun = mean,
+#                                        na.rm = TRUE,
+#                                        buffer = i)
+# 
+# }
+# 
+# ## Import the stem diameter data:
+# sdbh_rs <- raster(paste0(dir_rs, "sksSkogligaGrunddataDgv03.tif"))
+# 
+# ## Add the stem diameter to the extracted file:
+# for (i in buffer) {
+#   
+#   bol1 <- extracted$buffer == i
+#   extracted$sdbh_rs[bol1] <- extract(sdbh_rs,
+#                                      extracted[bol1, c("east", "north")],
+#                                      fun = mean,
+#                                      na.rm = TRUE,
+#                                      buffer = i)
+#   
+# }
+# 
+# ## Add "before" because no LiDAR data from after the experiment available:
+# extracted$experiment <- "before"
+#
+# dir.create("temp")
+# write.csv(extracted, "temp/forest_RS_uppland.csv", row.names = FALSE)
 extracted <- read.csv("temp/forest_RS_uppland.csv")
 
 ## 6. Merge data sets and export -----------------------------------------------
 
-forest_plot <- merge(f_plot, laser, by = c("plot", "experiment"))
+forest_plot <- merge(f_plot, 
+                     laser, 
+                     all.y = TRUE,
+                     by = c("plot", "experiment"))
 forest_plot <- merge(extracted[extracted$buffer == 50 & 
                                extracted$circle_10m == "middle", ],
                      forest_plot,
-                     by = "plot") 
+                     all.y = TRUE,
+                     by = c("plot", "experiment")) 
 
-forest_subplot <- merge(extracted, f_subplot, by = c("plot", "circle_10m")) 
+forest_subplot <- merge(extracted, 
+                        f_subplot, 
+                        all.y = TRUE,
+                        by = c("plot", "circle_10m", "experiment")) 
+
+## Add experimental treatment to both:
+forest_plot <- merge(forest_plot, 
+                     unique(ffmf[, c("plot", "treatment")]), 
+                     by = "plot")
+forest_subplot <- merge(forest_subplot, 
+                        unique(ffmf[, c("plot", "treatment")]), 
+                        by = "plot")
 
 dir.create("clean")
 write.csv(forest_plot, "clean/forest_data_uppland_plot.csv", row.names = FALSE)
