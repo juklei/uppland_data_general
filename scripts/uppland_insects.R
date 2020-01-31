c5306b35f250c6b5ef88c35ac53f2e99e31c7ffc
Julian Klein <jg.klein@protonmail.com>
2020-01-31 17:35:19 +0100

    insect data script running and working

--- scripts/uppland_insects.R
+++ scripts/uppland_insects.R
@@ -10,11 +10,38 @@
 rm(list = ls())
 
 library("data.table")
+library("ggplot2")
 
 ## 2. Define or source functions used in this script ---------------------------
 
+## This function copies specific comments between side a and b
+comment.exchange <- function(x) {
+  if("changed" %in% x) {return("changed")}
+  if("on_ground" %in% x) {return("on_ground")}
+}
+
+## This function standardises values to between 0 and 1
 standardise <- function(x) {
   (x - min(x, na.rm = T)) / (max(x, na.rm = T) - min(x, na.rm = T))
+}
+
+## THis funtion calculates the daily increment on the traps
+cover.calc <- function(x){
+  x <- x[order(x$date), ]
+  ## Calculate nr. off days difference:
+  for(i in 2:nrow(x)) {
+    x[i, "days_diff"] <- as.numeric(x[i, "date"] - x[i-1, "date"])
+  }
+  ## Calculate cover difference:
+  for(i in 2:nrow(x)){
+    if(x[i-1, "comment_both"] %in% c("changed", "on_ground")){
+      x[i, "daily_incr"] <- x[i, "mean_cover"] / x[i, "days_diff"]
+    } else{
+      x[i, "daily_incr"] <- ((x[i, "mean_cover"] - x[(i-1), "mean_cover"]) /
+                               (1 - x[(i-1), "mean_cover"])) / x[i, "days_diff"]
+    }
+  }
+  return(x)
 }
 
 ## 3. Load and explore data ----------------------------------------------------
@@ -88,9 +115,6 @@ rm(select)
 
 ## Remove all duplicated which are NA in cover and rerun code above:
 all_comb <- all_comb[!(all_comb$duplicated == "yes" & is.na(all_comb$cover)), ]
-select <- names(which(table(all_comb$id) > 1))
-all_comb$duplicated <- ifelse(all_comb$id %in% select, "yes", "no")
-rm(select)
 
 ## Export the file and clean it up trap by trap:
 all_comb <- all_comb[order(all_comb$id), ]
@@ -104,42 +128,85 @@ dir.create(paste0(pic_dir, "/renamed"))
 file.copy(from = paste0(pic_dir, "/", side_a$file_name),
           to = paste0(pic_dir, "/renamed/", new_files))
 
-## Now check for when new traps were raised and add/correct in 
-## insect_xxxx_cleanup.csv. Then reimport this file:
+## 5. Now check for when new traps were raised and add/correct in --------------
+##    insect_xxxx_cleaned.csv. Then reimport this file:
+ac_corr <- read.csv("temp/insect_2017_cleaned.csv")
 
-ac_corr <- read.csv("temp/insect_2017_cleanup.csv")
+## Define duplicates once more:
+select <- names(which(table(ac_corr$id) > 1))
+ac_corr$duplicated <- ifelse(ac_corr$id %in% select, "yes", "no")
+rm(select)
 
-## 5. Create a function that calculates the difference in % area between a  
+## Make sure the comments for a trap on a certain date are in the comment 
+## column for both sides:
+ac_corr <- as.data.table(ac_corr)
+ac_corr <- ac_corr[, "comment_both":= comment.exchange(comment), 
+                   by = c("plot", "trap", "date")]
+
+## Calculate the mean of all duplicates:
+ac_corr[, "mean_cover" := mean(cover, na.rm = TRUE), 
+        by = c("plot", "trap", "side", "date")]
+
+## What is the size of the residuals among the duplicates:
+dupl <- droplevels(ac_corr[ac_corr$duplicated == "yes", ])
+dupl$resid <- abs(dupl$mean_cover - dupl$cover)
+plot(density(dupl$resid, na.rm = TRUE))
+
+## 6. Create a function that calculates the difference in % area between a  
 ##    picture of a trap and the picture of the same trap at the preceeding visit
 
-ac_corr <- as.data.table(ac_corr)
-
-## Calculate the mean among all duplicates:
-#...
+## Keep only relevant columns, exclude duplicates and reformat date:
+acc_red <- unique(ac_corr[, c(1:4, 11, 10)])
+acc_red$date <- as.Date(acc_red$date, format = "%d/%m/%Y")
 
 ## Standardise the cover variable to the 0-1 range, to correct for the effect of 
 ## the person that did the analysis and year effects: 
-ac_corr$cover <- standardise(ac_corr$cover)
+acc_red$mean_cover <- standardise(acc_red$mean_cover)
 
-cover.calc <- function(x){
-  x <- x[order(x$date), ]
-  for(i in 2:nrow(x)){
-    x[i, "cover_diff"] <- (x[i, "cover"] - x[(i-1), "cover"]) /
-                          (1 - x[(i-1), "cover"])
-  }
-  return(x)
+## Calculate the daily increment:
+acc_red$days_diff <- as.numeric(NA)
+acc_red$daily_incr <- 0
+accr_incr <- acc_red[, cover.calc(.SD), by = c("plot", "trap", "side")]
+
+## All increments that are < 0 must become 0:
+accr_incr$daily_incr <- ifelse(accr_incr$daily_incr < 0,
+                               0,
+                               accr_incr$daily_incr)
+
+## Why so many (390) minus ????????
+
+## 6. Make different plots to evaluate the data quality ------------------------
+
+accr_incr$post_march <- as.numeric(accr_incr$date - as.Date("2017-03-31"))
+
+dir.create("figures")
+pdf("figures/insects_2017.pdf")
+for(i in levels(accr_incr$plot)){
+  G <- ggplot(droplevels(accr_incr[accr_incr$plot == i, ]), 
+              aes(post_march, daily_incr)) +#, color = plot)) +
+       geom_point() + 
+       geom_smooth(method = "lm", formula = y ~ poly(x, 2), se = FALSE) +
+       # theme(legend.position = "none")
+       ggtitle(print(i)) +
+       theme_classic(15)
+  plot(G)
 }
+dev.off()
 
-ac_corr$cover_diff <- 0
+## Make figure for cumultative cover with daily rate i for time (i-1):i
 
-
-
-
-
+## ...
 
 
-## 6. Analyse residuaals of duplicates to test methology -----------------------
 
+
+
+
+
+
+
+
+## -------------------------------END-------------------------------------------
 ...
 
 1) Merge names and measurments
@@ -154,4 +221,3 @@ without a model.
 7) To analyse only insect traps and how well they function, Download Lidar data for the traps coordinates and calculate forest 
 openness.
 
-## -------------------------------END-------------------------------------------
